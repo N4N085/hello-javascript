c64ab7aa569284ba9be94c62b4d3836dc2c5416f
N4N085 <78927755+N4N085@users.noreply.github.com>
2025-07-09 11:03:31 +0000

    Commit variables y hello world

--- Basic DPS/00-helloworld.js
+++ Basic DPS/00-helloworld.js
@@ -1,1 +1,17 @@
+// Esto es un comentario simple
+
+/* Esto es un 
+comentario multilínea */
+
 console.log("¡Hola, JavaScript!");
+console.log('¡Hola, JavaScript!');
+console.log(`¡Hola, JavaScript!`);
+
+console.log("5")
+console.log(5)
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 % 2)
+console.log(5 ** 2)
